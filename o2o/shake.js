2f74155db1beaec307b77fe9ede6925764a56af7
qijun88211 <54390018+qijun88211@users.noreply.github.com>
2019-09-26 09:19:21 +0800

    Create shake.js

---
+++ o2o/shake.js
@@ -0,0 +1,33 @@
+if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', deviceMotionHandler, false);
+}
+
+var isShake = false;
+
+function deviceMotionHandler(eventData) {
+      // 正在摇一摇
+      if (isShake) {
+          return false;
+      }
+
+      var acceleration = eventData.accelerationIncludingGravity;
+      var curTime = new Date().getTime();
+      if ((curTime - last_update) > 100) {
+          var diffTime = curTime - last_update;
+          last_update = curTime;
+          x = acceleration.x;
+          y = acceleration.y;
+          z = acceleration.z;
+          var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+          if (speed > SHAKE_THRESHOLD) { // 满足摇一摇条件
+              isShake = true;
+          }
+          
+          last_x = x;
+          last_y = y;
+          last_z = z;
+      }
+      
+      return isShake;
+  }
