89beb686b805ca38130e7a7f679cb8896134894b
qijun88211 <54390018+qijun88211@users.noreply.github.com>
2019-09-26 10:10:59 +0800

    Update shake.js

--- o2o/shake.js
+++ o2o/shake.js
@@ -1,38 +1,40 @@
-if (window.DeviceMotionEvent) {
-    alert(1);
-    window.addEventListener('devicemotion', deviceMotionHandler, false);
-}
-
-var isShake = false;
-
-function deviceMotionHandler(eventData) {
-    alert(2);
-    // 正在摇一摇
-    if (isShake) {
-        return false;
+define(function (require, exports, module) {
+    if (window.DeviceMotionEvent) {
+        alert(1);
+        window.addEventListener('devicemotion', deviceMotionHandler, false);
     }
 
-    var acceleration = eventData.accelerationIncludingGravity;
-    var curTime = new Date().getTime();
-    if ((curTime - last_update) > 100) {
-        var diffTime = curTime - last_update;
-        last_update = curTime;
-        x = acceleration.x;
-        y = acceleration.y;
-        z = acceleration.z;
-        var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+    var isShake = false;
 
-        if (speed > SHAKE_THRESHOLD) { // 满足摇一摇条件
-            alert(3);
-            isShake = true;
+    function deviceMotionHandler(eventData) {
+        alert(2);
+        // 正在摇一摇
+        if (isShake) {
+            return false;
         }
 
-        last_x = x;
-        last_y = y;
-        last_z = z;
-    }
-    
-    alert(4);
+        var acceleration = eventData.accelerationIncludingGravity;
+        var curTime = new Date().getTime();
+        if ((curTime - last_update) > 100) {
+            var diffTime = curTime - last_update;
+            last_update = curTime;
+            x = acceleration.x;
+            y = acceleration.y;
+            z = acceleration.z;
+            var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
 
-    return isShake;
-}
+            if (speed > SHAKE_THRESHOLD) { // 满足摇一摇条件
+                alert(3);
+                isShake = true;
+            }
+
+            last_x = x;
+            last_y = y;
+            last_z = z;
+        }
+
+        alert(4);
+
+        return isShake;
+    }
+});
